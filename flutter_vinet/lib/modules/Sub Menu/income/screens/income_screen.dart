d3d39a0a956abbe68f68a86843c48ddbcf7f6754
khalidrinaldy <alid.rinaldy@gmail.com>
2021-08-18 10:23:53 +0700

    Update Vinet 1.1

--- flutter_vinet/lib/modules/Sub Menu/income/screens/income_screen.dart
+++ flutter_vinet/lib/modules/Sub Menu/income/screens/income_screen.dart
@@ -64,8 +64,7 @@ class _IncomeScreenState extends State<IncomeScreen> {
           income = servers.elementAt(Provider.of<ServerProvider>(context).choosedIndex!).income;
           return Align(
             alignment: Alignment.center,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+            child: ListView(
               children: [
                 SizedBox(
                   height: 30,
@@ -98,82 +97,88 @@ class _IncomeScreenState extends State<IncomeScreen> {
                 ),
 
                 //MONTH-YEAR
-                Container(
-                  width: 160,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          color: Color(myColors.primarySoftGreen()).withOpacity(0.35),
-                          shape: BoxShape.circle,
+                Center(
+                  child: Container(
+                    width: 160,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: Color(myColors.primarySoftGreen()).withOpacity(0.35),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.keyboard_arrow_left_rounded,
+                            size: 20,
+                          ),
                         ),
-                        child: Icon(
-                          Icons.keyboard_arrow_left_rounded,
-                          size: 20,
+                        Text(
+                          "June 2021",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "June 2021",
-                        style: TextStyle(
-                          fontSize: 14,
+                        Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: Color(myColors.primarySoftGreen()).withOpacity(0.35),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.keyboard_arrow_right_rounded,
+                            size: 20,
+                          ),
                         ),
-                      ),
-                      Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          color: Color(myColors.primarySoftGreen()).withOpacity(0.35),
-                          shape: BoxShape.circle,
-                        ),
-                        child: Icon(
-                          Icons.keyboard_arrow_right_rounded,
-                          size: 20,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
 
                 //INCOME CHART
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.8 > 310 ? MediaQuery.of(context).size.width * 0.8 : 310,
-                  height: 90,
-                  child: chart(incomeDummys),
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8 > 310 ? MediaQuery.of(context).size.width * 0.8 : 310,
+                    height: 90,
+                    child: chart(incomeDummys),
+                  ),
                 ),
                 SizedBox(
                   height: 40,
                 ),
 
                 //INCOME CARD-1
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.8 > 310 ? MediaQuery.of(context).size.width * 0.8 : 310,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Widgets().incomeCard(
-                        color: Color(myColors.secondaryBlue()),
-                        icon: Icon(
-                          Icons.receipt_rounded,
-                          size: 28,
-                          color: Color(myColors.secondaryTextColor()),
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8 > 310 ? MediaQuery.of(context).size.width * 0.8 : 310,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Widgets().incomeCard(
+                          color: Color(myColors.secondaryBlue()),
+                          icon: Icon(
+                            Icons.receipt_rounded,
+                            size: 28,
+                            color: Color(myColors.secondaryTextColor()),
+                          ),
+                          title: "Total Bill",
+                          amount: "Rp.${moneyFormat.format(income!.totalBill)}",
                         ),
-                        title: "Total Bill",
-                        amount: "Rp.${moneyFormat.format(income!.totalBill)}",
-                      ),
-                      Widgets().incomeCard(
-                        color: Color(myColors.primaryBlue()),
-                        icon: Icon(
-                          Icons.receipt_long_rounded,
-                          size: 28,
-                          color: Color(myColors.secondaryTextColor()),
+                        Widgets().incomeCard(
+                          color: Color(myColors.primaryBlue()),
+                          icon: Icon(
+                            Icons.receipt_long_rounded,
+                            size: 28,
+                            color: Color(myColors.secondaryTextColor()),
+                          ),
+                          title: "Total Tax",
+                          amount: "Rp.${moneyFormat.format(income!.tax)}",
                         ),
-                        title: "Total Tax",
-                        amount: "Rp.${moneyFormat.format(income!.tax)}",
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -181,32 +186,34 @@ class _IncomeScreenState extends State<IncomeScreen> {
                 ),
 
                 //INCOME CARD-2
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.8 > 310 ? MediaQuery.of(context).size.width * 0.8 : 310,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Widgets().incomeCard(
-                        color: Color(myColors.thirdSoftGreen()),
-                        icon: Icon(
-                          Icons.money_off_rounded,
-                          size: 28,
-                          color: Color(myColors.secondaryTextColor()),
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8 > 310 ? MediaQuery.of(context).size.width * 0.8 : 310,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Widgets().incomeCard(
+                          color: Color(myColors.thirdSoftGreen()),
+                          icon: Icon(
+                            Icons.money_off_rounded,
+                            size: 28,
+                            color: Color(myColors.secondaryTextColor()),
+                          ),
+                          title: "Total Unpaid",
+                          amount: "Rp.${moneyFormat.format(income!.unpaid)}",
                         ),
-                        title: "Total Unpaid",
-                        amount: "Rp.${moneyFormat.format(income!.unpaid)}",
-                      ),
-                      Widgets().incomeCard(
-                        color: Color(myColors.primaryGreen()),
-                        icon: Icon(
-                          Icons.attach_money_rounded,
-                          size: 28,
-                          color: Color(myColors.secondaryTextColor()),
+                        Widgets().incomeCard(
+                          color: Color(myColors.primaryGreen()),
+                          icon: Icon(
+                            Icons.attach_money_rounded,
+                            size: 28,
+                            color: Color(myColors.secondaryTextColor()),
+                          ),
+                          title: "Total Paid",
+                          amount: "Rp.${moneyFormat.format(income!.paid)}",
                         ),
-                        title: "Total Paid",
-                        amount: "Rp.${moneyFormat.format(income!.paid)}",
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -214,22 +221,24 @@ class _IncomeScreenState extends State<IncomeScreen> {
                 ),
 
                 //INCOME CARD-3
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.8 > 310 ? MediaQuery.of(context).size.width * 0.8 : 310,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Widgets().incomeCard(
-                        color: Color(myColors.primaryYellow()),
-                        icon: Icon(
-                          Icons.attach_money_rounded,
-                          size: 28,
-                          color: Color(myColors.secondaryTextColor()),
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8 > 310 ? MediaQuery.of(context).size.width * 0.8 : 310,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Widgets().incomeCard(
+                          color: Color(myColors.primaryYellow()),
+                          icon: Icon(
+                            Icons.attach_money_rounded,
+                            size: 28,
+                            color: Color(myColors.secondaryTextColor()),
+                          ),
+                          title: "Managed\nServices",
+                          amount: "Rp.${moneyFormat.format(income!.managedServices)}",
                         ),
-                        title: "Managed\nServices",
-                        amount: "Rp.${moneyFormat.format(income!.managedServices)}",
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 )
               ],
